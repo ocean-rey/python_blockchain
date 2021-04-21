ea935227b31f055b9eede8dedf6ad6034879897b
ocean-rey <x7flux@gmail.com>
2021-04-21 04:40:31 +0300

    added difficult increment and kH/s calculation

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import time
 from hashlib import sha256
 
 class Blockchain:
-    def __init__(self):
+    def __init__(self, difficulty):
         self.chain = []
+        self.difficulty = difficulty
         self.chain.append(Block(0, "Genisis"))
 
     def AddBlock(self, bNew):
         bNew.prevHash = self.chain[-1].hash
-        bNew.MineBlock(5)
+        bNew.MineBlock(self.difficulty)
+        return
+
+    def IncDiff(self):
+        self.difficulty+= 1;
         return
 
 class Block:
@@ -27,27 +32,32 @@ class Block:
         hash.update(bytes(self.data, "utf-8"))
         hash.update(bytes(self.nonce))
         digest = hash.hexdigest()
-        #print(digest)
         return digest
 
     def MineBlock(self, difficulty):
+        start = time.time()
         compStr = ""
         for i in range(difficulty) :
             compStr = compStr+'0'
-        print(compStr)
         while True:
             self.nonce += 1;
             self.hash = self.CalculateHash()
             #print(self.hash[0:difficulty])
             if (self.hash[0:difficulty] == compStr):
                 break
-        print("Block mined:\t{}\nNonce:\t{}".format(self.hash, self.nonce))
+        stop = time.time()
+        duration = stop - start
+        print("Block mined:\t{}".format(self.hash))
+        print("Difficulty:\t{}".format(difficulty))
+        print("kH/s:\t{}".format((self.nonce/1000)/duration))
 
 def main():
-    blockchain = Blockchain()
-    blockchain.AddBlock(Block(1, "some data"))
-    blockchain.AddBlock(Block(2, "more data"))
-    blockchain.AddBlock(Block(3, "even more data"))
+    blockchain = Blockchain(1)
+    i = 1
+    while True:
+        blockchain.AddBlock(Block(i, "some data"))
+        blockchain.IncDiff()
+        i+=1
     return
 
 if __name__ == "__main__":
